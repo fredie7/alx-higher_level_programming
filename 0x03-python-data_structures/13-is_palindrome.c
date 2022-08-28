b85eb68cbac81df03a7c609e178e16f51bd4daac
fredie7 <lexfrederick3@gmail.com>
2022-08-28 10:31:32 -0700

    Tackle technical interview

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,7 +3,6 @@
 /**
  * is_palindrome - checks if a singly linked list is a palindrome.
  * @head: double pointer to start of linked list.
- * 
  * Return: 0 if not a palindrome, else return 1.
  */
 
