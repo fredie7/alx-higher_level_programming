e89c6586f027f16eb03861a32bb1c7796071d29d
fredie7 <lexfrederick3@gmail.com>
2022-08-28 10:26:36 -0700

    Tackle technical interview

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,72 +1,49 @@
 #include "lists.h"
 
-listint_t *reverse_listint(listint_t **head);
-int is_palindrome(listint_t **head);
-
 /**
- * reverse_listint - Reverses a singly-linked listint_t list.
- * @head: A pointer to the starting node of the list to reverse.
- *
- * Return: A pointer to the head of the reversed list.
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * @head: double pointer to start of linked list.
+ * 
+ * Return: 0 if not a palindrome, else return 1.
  */
-listint_t *reverse_listint(listint_t **head)
-{
-	listint_t *node = *head, *next, *prev = NULL;
 
-	while (node)
-	{
-		next = node->next;
-		node->next = prev;
-		prev = node;
-		node = next;
-	}
-
-	*head = prev;
-	return (*head);
-}
-
-/**
- * is_palindrome - Checks if a singly linked list is a palindrome.
- * @head: A pointer to the head of the linked list.
- *
- * Return: If the linked list is not a palindrome - 0.
- *         If the linked list is a palindrome - 1.
- */
 int is_palindrome(listint_t **head)
 {
-	listint_t *tmp, *rev, *mid;
-	size_t size = 0, i;
-
-	if (*head == NULL || (*head)->next == NULL)
-		return (1);
-
-	tmp = *head;
-	while (tmp)
-	{
-		size++;
-		tmp = tmp->next;
-	}
-
-	tmp = *head;
-	for (i = 0; i < (size / 2) - 1; i++)
-		tmp = tmp->next;
-
-	if ((size % 2) == 0 && tmp->n != tmp->next->n)
-		return (0);
-
-	tmp = tmp->next->next;
-	rev = reverse_listint(&tmp);
-	mid = rev;
-
-	tmp = *head;
-	while (rev)
-	{
-		if (tmp->n != rev->n)
-			return (0);
-		tmp = tmp->next;
-		rev = rev->next;
-	}
-	reverse_listint(&mid);
-
-	return (1);
+listint_t *slow = *head, *fast = *head, *node, *prev;
+int failed = 0;
+while (fast != NULL && fast->next != NULL)
+{
+fast = fast->next->next;
+slow = slow->next;
+}
+node = slow;
+prev = NULL;
+while (node)
+{
+fast = node->next;
+node->next = prev;
+prev = node;
+node = fast;
+}
+fast = *head;
+node = prev;
+while (prev)
+{
+if (fast->n != prev->n)
+{
+failed = 1;
+break;
+}
+fast = fast->next;
+prev = prev->next;
+}
+prev = NULL;
+while (node)
+{
+fast = node->next;
+node->next = prev;
+prev = node;
+node = fast;
+}
+return (!failed);
 }
